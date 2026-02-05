b040331cac2ec448a51171e219bee2960386d193
abhishek <abhisheksony35922@gmail.com>
2026-02-05 14:53:40 +0530

    Initial Spring Boot setup for GRC Risk Tool

---
+++ risktool/src/main/java/com/grc/risktool/GrcRiskToolAbhishekPrasadApplication.java
@@ -0,0 +1,13 @@
+package com.grc.risktool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrcRiskToolAbhishekPrasadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrcRiskToolAbhishekPrasadApplication.class, args);
+	}
+
+}
